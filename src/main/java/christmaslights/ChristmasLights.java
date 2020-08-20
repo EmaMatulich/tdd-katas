8f11d578f870b6d34c3ab062636697d8670005ef
ematulich <emanuel.matulich@mercadolibre.com>
2020-08-20 00:17:51 -0300

    final refactor

--- src/main/java/christmaslights/ChristmasLights.java
+++ src/main/java/christmaslights/ChristmasLights.java
@@ -4,10 +4,6 @@ import java.util.List;
 
 public class ChristmasLights {
 
-    private static final String TURN_ON_INSTRUCTION = "turn on";
-    private static final String TURN_OF_INSTRUCTION = "turn of";
-    private static final String TOGGLE_INSTRUCTION = "toggle";
-
     private int lightsOn;
     private boolean[][] lights;
 
@@ -22,47 +18,59 @@ public class ChristmasLights {
 
     public void processInstructions(List<String> instructions){
         for (String instruction: instructions){
-            Instruction instructionCommand = new Instruction(instruction);
-            process(instructionCommand);
+            process(new Instruction(instruction));
         }
     }
 
     private void process(Instruction command) {
-        if (command.getCommand().equals(InstructionCommand.TURN_ON)){
-            for  (int i = command.getStartingRow(); i<= command.getEndingRow(); i++){
-                for (int j = command.getStartingColumn(); j<= command.getEndingColumn(); j++){
-                    if (!lights[i][j]){
-                        lights[i][j] = true;
-                        lightsOn++;
-                    }
-                }
-            }
-        }
-        if (command.getCommand().equals(InstructionCommand.TURN_OFF)){
-            for  (int i = command.getStartingRow(); i<= command.getEndingRow(); i++){
-                for (int j = command.getStartingColumn(); j<= command.getEndingColumn(); j++){
-                    if (lights[i][j]){
-                        lights[i][j] = false;
-                        lightsOn--;
-                    }
-                }
-            }
-        }
+        if (command.getCommand().equals(InstructionCommand.TURN_ON))
+            processTurnOnCommand(command);
+        if (command.getCommand().equals(InstructionCommand.TURN_OFF))
+            processTurnOffCommand(command);
+        if (command.getCommand().equals(InstructionCommand.TOGGLE))
+            processToggleCommand(command);
+    }
 
-        if (command.getCommand().equals(InstructionCommand.TOGGLE)){
-            for  (int i = command.getStartingRow(); i<= command.getEndingRow(); i++){
-                for (int j = command.getStartingColumn(); j<= command.getEndingColumn(); j++){
-                    if (lights[i][j]){
-                        lights[i][j] = false;
-                        lightsOn--;
-                    }else {
-                        lights[i][j] = true;
-                        lightsOn++;
-                    }
+    private void processTurnOnCommand(Instruction command) {
+        for  (int i = command.getStartingRow(); i<= command.getEndingRow(); i++){
+            for (int j = command.getStartingColumn(); j<= command.getEndingColumn(); j++){
+                turnOnLight(i, j);
+            }
+        }
+    }
+
+    private void processTurnOffCommand(Instruction command) {
+        for  (int i = command.getStartingRow(); i<= command.getEndingRow(); i++){
+            for (int j = command.getStartingColumn(); j<= command.getEndingColumn(); j++){
+                turnOffLight(i, j);
+            }
+        }
+    }
+
+    private void processToggleCommand(Instruction command) {
+        for  (int i = command.getStartingRow(); i<= command.getEndingRow(); i++){
+            for (int j = command.getStartingColumn(); j<= command.getEndingColumn(); j++){
+                if (lights[i][j]){
+                    turnOffLight(i,j);
+                }else {
+                   turnOnLight(i,j);
                 }
             }
         }
+    }
 
+    private void turnOnLight(int row, int column) {
+        if (!lights[row][column]){
+            lights[row][column] = true;
+            lightsOn++;
+        }
+    }
+
+    private void turnOffLight(int row, int column) {
+        if (lights[row][column]){
+            lights[row][column] = false;
+            lightsOn--;
+        }
     }
 
 
